5c230f166445c40132467781ee97cfc0b5016f6c
Pasquale Lodise <pasquale.lodise@nttdata.com>
2022-02-28 16:01:15 +0100

    Variuos implementations

--- lib/modules/termzilla.ssh/utils/termzilla.ssh.terminalbackend.dart
+++ lib/modules/termzilla.ssh/utils/termzilla.ssh.terminalbackend.dart
@@ -3,20 +3,24 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:legacy_progress_dialog/legacy_progress_dialog.dart';
+import 'package:termzilla/shared/model/termzilla.connectioninfo.model.dart';
 import 'package:xterm/xterm.dart';
 import 'package:dartssh2/dartssh2.dart';
 
 class SSHTerminalBackend extends TerminalBackend {
   late SSHClient client;
-
-  String _host;
-  String _username;
-  String _password;
+  ConnectionInfo _connectionInfo;
+  BuildContext _parentBuildContext;
 
   final _exitCodeCompleter = Completer<int>();
   final _outStream = StreamController<String>();
 
-  SSHTerminalBackend(this._host, this._username, this._password);
+  SSHTerminalBackend(
+      ConnectionInfo connectionInfo, BuildContext parentBuildContext)
+      : _connectionInfo = connectionInfo,
+        _parentBuildContext = parentBuildContext;
 
   void onWrite(String data) {
     _outStream.sink.add(data);
@@ -34,15 +38,28 @@ class SSHTerminalBackend extends TerminalBackend {
     final _sshOutput = StreamController<List<int>>();
     _sshOutput.stream.transform(utf8.decoder).listen(onWrite);
 
-    onWrite('connecting $_host...');
-    client = SSHClient(await SSHSocket.connect('10.143.91.16', 22),
-        username: 'azureadmin',
-        onPasswordRequest: () => 'Welcome123!',
-        onAuthenticated: () => onWrite("connected"));
-    shell = await client.shell();
+    ProgressDialog pd = ProgressDialog(
+      context: _parentBuildContext,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+      loadingText: "Trying to connect to ${_connectionInfo.ipAddress}",
+    );
+    pd.show();
+    try {
+      client = SSHClient(
+          await SSHSocket.connect(
+              _connectionInfo.ipAddress, int.parse(_connectionInfo.port),
+              timeout: const Duration(seconds: 15)),
+          username: _connectionInfo.username,
+          onPasswordRequest: () => _connectionInfo.password,
+          onAuthenticated: () => pd.dismiss());
+      shell = await client.shell();
+    } on SocketException catch (e) {
+      pd.dismiss();
+    }
+
     _sshOutput.addStream(shell.stdout); // listening for stdout
     _sshOutput.addStream(shell.stderr); // listening for stderr
-
     // client = SSHClient(
     //   hostport: Uri.parse(_host),
     //   username: _username,
@@ -71,7 +88,7 @@ class SSHTerminalBackend extends TerminalBackend {
 
   @override
   void resize(int width, int height, int pixelWidth, int pixelHeight) {
-    shell.resizeTerminal(width, height, pixelWidth, pixelHeight);
+    //  shell.resizeTerminal(width, height, pixelWidth, pixelHeight);
   }
 
   @override
